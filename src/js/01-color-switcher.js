5a368c2696cafe99f017a92355ec491c1db75dae
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-01-19 22:08:39 +0200

    task#1
    Добавляет решение задачи №1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.body;
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+      body.style.background = getRandomHexColor();
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+      stopBtn.disabled = true;
+  
+});
