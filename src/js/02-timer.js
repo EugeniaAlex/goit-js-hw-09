80a17c821cf287a3204bc051a340e0aa45f3dcd7
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-01-21 20:24:43 +0200

    task#3
    Добавляет решение задачи №3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,38 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+let countTime = null;
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minsEl = document.querySelector('span[data-minutes]');
+const secsEl = document.querySelector('span[data-seconds]');
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    
+    const selectedDate = selectedDates[0].getTime();
+    const currentDate = options.defaultDate.getTime();
+    countTime = selectedDate - currentDate;
+
+    if (countTime < 0) {
+      startBtn.disabled = true;
+      window.alert("Please choose a date in the future");
+      return;
+    }
+
+    startBtn.disabled = false;   
   },
 };
 
-const input = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('button[data-start]');
+
 const divTimerEl = document.querySelector('.timer');
 const spanEl = document.querySelectorAll('span');
 const spanValueEl = document.querySelectorAll('.value');
@@ -45,6 +65,63 @@ divFieldEl.forEach(div => {
 
 flatpickr(input, options);
 
-console.log(options.defaultDate)
+// console.log(options.defaultDate);
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive && startBtn.disabled === true) {
+      return;
+    }
+
+    const startTime = countTime;
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      countTime -= 1000;
+      const time = convertMs(countTime);
+      updateTimer(time);
+      this.stop();
+     }, 1000);
+  },
+  stop() {
+    if (daysEl.textContent === '00' && hoursEl.textContent === '00' &&
+      minsEl.textContent === '00' && secsEl.textContent === '00') {
+      
+      clearTimeout(this.intervalId);
+      }
+  },
+ };
+
+startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+function updateTimer({ days, hours, minutes, seconds }) { 
+  daysEl.textContent = `${days}`.padStart(2, '0');
+  hoursEl.textContent = `${hours}`.padStart(2, '0');
+  minsEl.textContent = `${minutes}`.padStart(2, '0');
+  secsEl.textContent = `${seconds}`.padStart(2, '0');
+
+  timer.stop(daysEl.textContent, hoursEl.textContent, minsEl.textContent, secsEl.textContent);
+
+};
+
     
+function convertMs(ms) {
+  
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+
+
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,76 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const creatBtn = document.querySelector('.form button');
+
+form.addEventListener('submit', getInputValue);
+
+function getInputValue(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+  if (delay.value !== "" && step.value !== "" && amount.value !== "") {
+    
+    const inputVales = {
+      startDelay: `${delay.value}`,
+      delay: `${step.value}`,
+      number: `${amount.value}`,
+    };
+    creatPromisesChaning(inputVales);
+  } 
+  else {
+    window.alert("Все поля должны быть заполнены");
+  };
+  event.currentTarget.reset();
+};
+  
+   
+
+function creatPromisesChaning( {startDelay, delay, number} ) { 
+  
+  
+  // let totalDelay = 0;
+
+  // if (startDelay >= delay) {
+  //   totalDelay = startDelay - delay;
+  // }
+  // else {
+  //   totalDelay = delay - startDelay;
+  // };
+
+  setTimeout(() => {
+    for (let i = 0; i < number; i += 1) { 
+
+      createPromise(i, delay)
+         .then(result => Notify.success(result))
+        .catch(error => Notify.failure(error));
+      
+        
+    };
+
+  }, startDelay)
+
+  
+
+};
+
+function createPromise(position, delay) { 
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      
+      if (shouldResolve) { 
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+
+      else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }  
+    }, delay)
+   });
+};
+
+
